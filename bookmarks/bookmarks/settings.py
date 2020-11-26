20a6d4c4dbc078afb92dae59a3430a690c7f9d4b
rakesh <davathrak@gmail.com>
2020-11-26 20:02:22 +0530

    changed settings

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
